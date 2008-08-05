b0050cd5240ba3acc4648fb7bd91735df1958ee4
jamiroconca <conca.daniele@gmail.com>
2008-08-05 00:37:36 +0000

    language baseline

---
+++ modules/crpTag/pnlang/ita/moduletags.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * crpTag
+ *
+ * @copyright (c) 2007, Daniele Conca
+ * @link http://noc.postnuke.com/projects/crptag Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
+ * @package crpTag
+ */
+
+define('_CRPTAG_TAGS','Elenco tag');
+define('_CRPTAG_MODULE','Tag del modulo');
