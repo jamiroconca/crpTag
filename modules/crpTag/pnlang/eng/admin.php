68423fcc5253c9acb6803fb5ac71825471322a95
jamiroconca <conca.daniele@gmail.com>
2008-08-20 16:40:49 +0000

    languages updates

--- modules/crpTag/pnlang/eng/admin.php
+++ modules/crpTag/pnlang/eng/admin.php
@@ -2,7 +2,7 @@
 /**
  * crpTag
  *
- * @copyright (c) 2007, Daniele Conca
+ * @copyright (c) 2008 Daniele Conca
  * @link http://noc.postnuke.com/projects/crptag Support and documentation
  * @author Daniele Conca <conca.daniele@gmail.com>
  * @license GNU/GPL - v.2.1
@@ -14,8 +14,10 @@ define('_CRPTAG_TAGS','Tags list');
 
 // form
 define('_CRPTAG_TAGS_INSERT','Tags (comma separated)');
+define('_CRPTAG_TAGS_TIPYING_CHARS','Type some chars and already used tags will be suggested');
 
 // config
 define('_CRPTAG_GENERAL','General settings');
 define('_CRPTAG_MINLENGTH','Min. length(shorter tag will not be stored)');
+define('_CRPTAG_USE_AJAX','Use Ajax insert');
 define('_CRPTAG_PURGE_ARCHIVE','Purge unused tags from database');
--- modules/crpTag/pnlang/ita/admin.php
+++ modules/crpTag/pnlang/ita/admin.php
@@ -2,7 +2,7 @@
 /**
  * crpTag
  *
- * @copyright (c) 2007, Daniele Conca
+ * @copyright (c) 2008 Daniele Conca
  * @link http://noc.postnuke.com/projects/crptag Support and documentation
  * @author Daniele Conca <conca.daniele@gmail.com>
  * @license GNU/GPL - v.2.1
@@ -14,8 +14,10 @@ define('_CRPTAG_TAGS','Elenco tags');
 
 // form
 define('_CRPTAG_TAGS_INSERT','Tags (separati da virgole)');
+define('_CRPTAG_TAGS_TIPYING_CHARS','Scrivendo alcune lettere verranno presentati una serie di tag usati precedentemente');
 
 // config
 define('_CRPTAG_GENERAL','Impostazioni generali');
 define('_CRPTAG_MINLENGTH','Lunghezza minima(i tag brevi non verranno memorizzati)');
+define('_CRPTAG_USE_AJAX','Utilizza inserimenti Ajax');
 define('_CRPTAG_PURGE_ARCHIVE','Elimina tags non utilizzati dal database');
