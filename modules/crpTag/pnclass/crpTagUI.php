87a24e36564df0736f5796c931164c3a5e3df294
jamiroconca <conca.daniele@gmail.com>
2009-01-08 15:48:44 +0000

    file header update

--- modules/crpTag/pnclass/crpTagUI.php
+++ modules/crpTag/pnclass/crpTagUI.php
@@ -3,7 +3,7 @@
 /**
  * crpTag
  *
- * @copyright (c) 2008 Daniele Conca
+ * @copyright (c) 2008-2009 Daniele Conca
  * @link http://code.zikula.org/crptag Support and documentation
  * @author Daniele Conca <conca.daniele@gmail.com>
  * @license GNU/GPL - v.2.1
