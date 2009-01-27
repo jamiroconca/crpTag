a7fd17e5cb14555f60c30a4a1974c0a72df25285
jamiroconca <conca.daniele@gmail.com>
2009-01-27 11:43:55 +0000

    new tag sphere block

---
+++ modules/crpTag/pnlang/eng/tagsphere.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * crpTag
+ *
+ * @copyright (c) 2008-2009 Daniele Conca
+ * @link http://code.zikula.org/crptag Support and documentation
+ * @author Daniele Conca <conca.daniele@gmail.com>
+ * @license GNU/GPL - v.2.1
+ * @package crpTag
+ */
+
+Loader :: includeOnce('modules/crpTag/pnlang/eng/global_block.php');
+
+define('_CRPTAG_SPHERE_WIDTH','Sphere width');
+define('_CRPTAG_SPHERE_HEIGHT','Sphere height');
